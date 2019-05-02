7bf755c73feb584423e81d386d747ad7f0696229
Isaac Martinez <cf17isaac.martinez@iesjoandaustria.org>
2019-05-02 18:20:20 +0200

    FirstCommit

---
+++ app/src/main/java/com/example/isaac/m8uf2provafinal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.isaac.m8uf2provafinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
