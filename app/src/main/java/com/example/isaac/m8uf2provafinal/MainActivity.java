097d3bc0074cc3fbaae12402155537879d7e548e
Isaac Martinez <cf17isaac.martinez@iesjoandaustria.org>
2019-05-02 18:31:59 +0200

    Toolbar done

--- app/src/main/java/com/example/isaac/m8uf2provafinal/MainActivity.java
+++ app/src/main/java/com/example/isaac/m8uf2provafinal/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.isaac.m8uf2provafinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Toolbar toolbar;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
