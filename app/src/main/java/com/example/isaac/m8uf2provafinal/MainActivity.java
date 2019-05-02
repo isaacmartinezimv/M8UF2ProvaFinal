a4d3d91810016a20257221c6dcd47c03fe63fe5e
Isaac Martinez <cf17isaac.martinez@iesjoandaustria.org>
2019-05-02 18:42:34 +0200

    menu created and fragments created

--- app/src/main/java/com/example/isaac/m8uf2provafinal/MainActivity.java
+++ app/src/main/java/com/example/isaac/m8uf2provafinal/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.isaac.m8uf2provafinal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,28 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.musicItem:
+                Toast.makeText(this, "MusicaPlay", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item1:
+                Toast.makeText(this, "item1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item2:
+                Toast.makeText(this, "item2", Toast.LENGTH_SHORT).show();
+                break;
+        }
+            return super.onOptionsItemSelected(item);
+    }
+
 }
